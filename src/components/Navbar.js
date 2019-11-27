dc7e26b3fd2085aeae9018095f3a398ae1215678
Sarah Wilsoncook <joonko_user@JK00046.local>
2019-11-27 17:29:54 +0100

    changing components to functional components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,33 +1,24 @@
-import React, { Component } from 'react';
-import { ThemeContext } from '../contexts/ThemeContext';
-import { AuthContext } from '../contexts/AuthContext';
+import React, { useContext } from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
+import { AuthContext } from "../contexts/AuthContext";
 
-class Navbar extends Component {
-    render() { 
-        return ( 
-            <AuthContext.Consumer>{(authContext) => (
-                <ThemeContext.Consumer>{(themeContext) => {
-                    const { isAutheticated, toggleAuth } = authContext;
-                    const { isLightTheme, light, dark } = themeContext;
-                    const theme = isLightTheme ? light : dark;
-                    return (
-                        <nav style={{ background: theme.ui, color: theme.syntax }}>
-                            <h1>Context App</h1>
-                            <div onClick={toggleAuth}>
-                                { isAutheticated ? 'Logged In' : 'Logged Out' }
-                            </div>
-                            <ul>
-                                <li>Home</li>
-                                <li>About</li>
-                                <li>Contact</li>
-                            </ul>
-                        </nav>
-                    )
-                }}
-                </ThemeContext.Consumer>
-            )}</AuthContext.Consumer>
-         );
-    }
-}
- 
+const Navbar = () => {
+  const { isLightTheme, light, dark } = useContext(ThemeContext);
+  const { isAutheticated, toggleAuth } = useContext(AuthContext);
+  const theme = isLightTheme ? light : dark;
+  return (
+    <nav style={{ background: theme.ui, color: theme.syntax }}>
+      <h1>Context App</h1>
+      <div onClick={toggleAuth}>
+        {isAutheticated ? "Logged In" : "Logged Out"}
+      </div>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+};
+
 export default Navbar;
